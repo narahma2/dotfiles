a56a47acb7ccc91d4377ac66b0bd3c194df75160
Naveed Rahman <>
2023-04-07 13:50:44 -0500

    New author mode

--- init_dotfiles.sh
+++ init_dotfiles.sh
@@ -2,3 +2,6 @@
 # Only need to run this once
 git submodule init
 git submodule update
+
+# Command to add in a new submodule:
+# git submodule add git@github.com:user/repo.git vim/pack/vendor/opt/repo
